7166bda0cbab0872022b0ca1cb8ae174f6bb6937
leiggee <leiggee@126.com>
2016-07-23 07:38:31 +0800

    核心代码0.0.2

---
+++ XHSDW/XHSDW/SDW/SDViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDViewController.h
+//  XHSDW
+//
+//  Created by apple on 16/7/23.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDViewController : UIViewController
+- (void)demo;
+@end
---
+++ XHSDW/XHSDW/SDW/SDViewController.m
@@ -0,0 +1,40 @@
+//
+//  SDViewController.m
+//  XHSDW
+//
+//  Created by apple on 16/7/23.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import "SDViewController.h"
+
+@interface SDViewController ()
+
+@end
+
+@implementation SDViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (void)demo
+{
+    NSLog(@"我是核心代码");
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
